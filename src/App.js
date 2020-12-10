f45f046894f385631ba7277a02c032c01b8c31a0
chris <chris.espinal92@hotmail.com>
2020-12-10 14:40:27 -0500

    installed missing packages and added navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './chris.png';
 // import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Home from './components/Home';
 import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
@@ -8,16 +9,57 @@ import ContactMe from './components/ContactMe';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-      <AboutMe/>
-      <Projects/>
-      <ContactMe/>
-    </div>
+    <Router>
+      <div className="App">
+      {/* <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>  
+          <li>
+            <Link to="/about">AboutMe</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/contact">ContactMe</Link>
+          </li>
+        </ul>
+      </nav>   */}
+
+     <div>
+       <Link to="/">Home </Link>
+       <Link to="/about">AboutMe </Link>
+       <Link to="/projects">Projects </Link>
+       <Link to="/contact">ContactMe </Link>
+     </div>
+
+
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/about" component={AboutMe}/>
+          <Route path="/projects" component={Projects}/>
+          <Route path="/contact" component={ContactMe}/>
+        </Switch>
+
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+// function Navbar() {
+//   return (
+//     <div>
+//       <Link to="/">Home </Link>
+//       <Link to="/about">AboutMe </Link>
+//       <Link to="/projects">Projects </Link>
+//       <Link to="/contact">ContactMe </Link>
+//     </div>
+//   );
+// };
 
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import styled, { keyframes } from 'syled-components';
+// import{ bounce } from 'react-animations';
+
+// const Bounce = styled.div`animation: 2s ${keyframes`${bounce}`} infinite`;
 
 class Home extends React.Component{
 
@@ -6,6 +10,7 @@ class Home extends React.Component{
         return(
             <div className = "Home">
                 <h1>This is my 'Home' component test</h1>
+                {/* <Bounce>Welcome!</Bounce> */}
             </div>
         );
     }
