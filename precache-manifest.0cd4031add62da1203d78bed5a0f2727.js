9fa2cb113c7c4211806e5299ac6290f6cf117ed4
chris <chris.espinal92@hotmail.com>
2020-12-19 00:44:01 -0500

    Updates

---
+++ precache-manifest.0cd4031add62da1203d78bed5a0f2727.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a24be7e188997493320fcd30780a103",
+    "url": "/index.html"
+  },
+  {
+    "revision": "481ba507b99f2af5a365",
+    "url": "/static/css/main.f6c983ba.chunk.css"
+  },
+  {
+    "revision": "c84ca88dc5d3527156ce",
+    "url": "/static/js/2.48908d16.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.48908d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "481ba507b99f2af5a365",
+    "url": "/static/js/main.15f55420.chunk.js"
+  },
+  {
+    "revision": "4f2995ce30e082268638",
+    "url": "/static/js/runtime-main.4fa9fcfc.js"
+  },
+  {
+    "revision": "23c8ede65d5fedd5c493d4a787577822",
+    "url": "/static/media/chris.23c8ede6.png"
+  },
+  {
+    "revision": "d6f7874b8b30e11b761624ba0a8a6a06",
+    "url": "/static/media/world.d6f7874b.png"
+  }
+]);
